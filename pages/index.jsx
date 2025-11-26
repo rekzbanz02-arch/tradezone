4d0196d369ba4d5637b24c2f920d08ae60b30d54
rekzbanz <rekzbanz02@gmail.com>
2025-11-27 00:34:21 +0800

    Fix alert syntax in handleSubmit function

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(Signup submitted for ${form.name});
+    alert('Signup submitted for ${form.name}');
   };
 
   const categories = [
