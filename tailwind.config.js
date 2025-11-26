17c96a69a6c7a3882dc3a48774e14b213c4372ea
rekzbanz <rekzbanz02@gmail.com>
2025-11-26 12:22:34 +0800

    Added TradeZone React site

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+      content: [
+            "./pages/**/*.{js,jsx}",
+                "./components/**/*.{js,jsx}"
+      ],
+        theme: {
+                extend: {},
+        },
+          plugins: [],
+};
+        }
+      ]
+}
